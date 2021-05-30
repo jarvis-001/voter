bb52054026a659823449acc8df8370a6ea53ad51
kishu <ghanshyammoyal058@gmail.com>
2021-05-30 14:39:33 +0530

    doing good till now

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,24 +1,3 @@
-// pragma solidity 0.5.16;
-// contract Election {
-//     // Model a Candidate
-//     struct Candidate {
-//         uint id;
-//         string name;
-//         uint voteCount;
-//     }
-//     // Read/write candidates
-//     mapping(uint => Candidate) public candidates;
-//     // Store Candidates Count
-//     uint public candidatesCount;
-//     constructor () public {
-//         addCandidate("Donald Trump");
-//         addCandidate("Narendra Modi");
-//     }
-//     function addCandidate (string memory _name) private {
-//         candidatesCount ++;
-//         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-//     }
-// }
 pragma solidity 0.5.16;
 contract Election {
     // Model a Candidate
